2e8fb83ec052db29ad101c6e05afe9c4f0429ab2
MiguelNdeCarvalho <miguel@cypheros.co>
2018-06-30 17:41:17 +0100

    whyred: AOSCP Bringup

--- device.mk
+++ device.mk
@@ -17,11 +17,7 @@
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
-
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
+    $(LOCAL_PATH)/overlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
